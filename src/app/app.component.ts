7945d8c5a5f88fead2dbf4a1b50df70b1f972bc7
Behroozi0149 <tahabehroozi298@gmail.com>
2024-07-15 13:43:58 +0330

    calculator created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalcComponent } from './calc/calc.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CalcComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,45 @@
+// ts codes
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calc',
+  standalone: true,
+  imports: [],
+  templateUrl: './calc.component.html',
+  styleUrl: './calc.component.css'
+})
+export class CalcComponent {
+  clear() {
+    this.mainValue = '0';
+    this.prevValue = '0';
+    this.operator = '';
+
+  }
+  digit(d: string) {
+    this.mainValue = Number(this.mainValue + d).toString();
+  }
+  op() {
+    if (this.operator == '+') {
+      this.mainValue = (Number(this.prevValue) + Number(this.mainValue)).toString();
+    }
+    else if (this.operator == '-') {
+      this.mainValue = (Number(this.prevValue) - Number(this.mainValue)).toString();
+    }
+    else if (this.operator == 'x') {
+      this.mainValue = (Number(this.prevValue) * Number(this.mainValue)).toString();
+    }
+    else if (this.operator == '/') {
+      this.mainValue = (Number(this.prevValue) / Number(this.mainValue)).toString();
+    }
+  }
+  operate(o: string) {
+    this.operator = o;
+    this.prevValue = Number(this.mainValue).toString();
+    this.mainValue = '0';
+
+  }
+  prevValue: string = '0';
+  mainValue: string = '0';
+  operator: string = '';
+
+}
